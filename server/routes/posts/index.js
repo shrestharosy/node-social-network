c2d10d38cdbe420807d547b6baca58050934a6a4
Rosy <rosyshrestha028@gmail.com>
2021-02-04 12:10:24 +0545

    Create rute to view posts

--- server/routes/posts/index.js
+++ server/routes/posts/index.js
@@ -1,54 +1,47 @@
-const express = require('express')
-const multer = require('multer')
-const uuid = require('uuid')
-const crud = require('./crud')
+const express = require('express');
+const crud = require('./crud');
+const postMiddlewares = require('../../middlewares/posts');
 
-const router = express.Router()
+const router = express.Router();
 
 class InvalidFileError extends Error {}
 
-const storage = multer.diskStorage({
-  destination: './public/upload',
-  filename: (req, file, cb) => {
-    cb(null, file.fieldname + '-' + Date.now() + uuid.v4())
-  }
-})
-
-const upload = multer({
-  storage,
-  fileFilter: (req, file, cb) => {
-    const fileIsValid = (
-      file.mimetype.startsWith('/text') || ['application/javascript', 'application/json'].includes(file.mimetype)
-    )
-    if (!fileIsValid) {
-      console.log(`Unsupported file type uploaded ${file.mimetype}`)
-      cb(new InvalidFileError('Upload file must be a text file'))
-    } else cb(null, true)
-  }
-})
-
-router.post('/', upload.single('codeFile'), async (req, res, next) => {
-  try {
-    if(!req.body.message) {
-      res.status(400).send({ error: 'Message field is required.' })
-      return
-    }
+router.post(
+  '/',
+  postMiddlewares.upload.single('codeFile'),
+  async (req, res, next) => {
+    try {
+      if (!req.body.message) {
+        res.status(400).send({ error: 'Message field is required.' });
+        return;
+      }
       const createdPost = await crud.createPost(
-      req.body.message,
+        req.body.message,
         req.file ? req.file.filename : null
-      )
-    res.json(createdPost)
-  } catch (e) {
-      next(e)
+      );
+      res.json(createdPost);
+    } catch (e) {
+      next(e);
+    }
   }
-})
+);
+
+router.get('/', async (req, res, next) => {
+  try {
+    const { page, limit } = req.query;
+    const posts = await crud.getPosts(page, limit);
+    res.json(posts);
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.use((err, req, res, next) => {
   if (err instanceof InvalidFileError) {
-    res.status(400).json({ error: err.message})
+    res.status(400).json({ error: err.message });
   } else {
-    next(err)
+    next(err);
   }
-})
+});
 
-module.exports = router
+module.exports = router;
